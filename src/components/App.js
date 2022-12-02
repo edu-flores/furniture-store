5078f13b28311636c861e2f4bc34b6811b2583e7
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-02 14:02:26 -0600

    Position footer at the bottom

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <main className='main'></main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      &nbsp;
+    </footer>
+  )
+}
+
+export default Footer;
