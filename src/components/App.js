2598b351a5cd31beffacfa86b4ff796188a0519f
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-02 18:41:56 -0600

    Display main info

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,13 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <main className='main'></main>
+      <main className='main'>
+        <h1 className='main--header'>What items to store?</h1>
+        <p className='main--info'>
+          Select which items you wish to store before moving to your new home. We'll keep 'em safe!
+        </p>
+        <h2 className='main--summary'>Summary</h2>
+      </main>
       <Footer />
     </div>
   );
