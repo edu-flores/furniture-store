38742b37c897ce0dae0abff199dfbe46ef269715
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-04 13:11:11 -0600

    Center navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header grid grid-cols-12 gap-y-5 lg:gap-y-0">
       <img className="col-span-12 md:col-span-4" src={Logo} alt="Logo" />
-      <nav className="col-span-12 md:col-span-8 header--nav">
+      <nav className="col-span-12 md:col-span-8 header--nav flex items-center">
         <ul className="header--list grid grid-cols-4 gap-y-5 lg:gap-y-0 lg:gap-x-10">
           <li className="col-span-4 lg:col-span-1">How We Work</li>
           <li className="col-span-4 lg:col-span-1">Services</li>
